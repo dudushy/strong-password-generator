48b6185b0b0c7703cf908fd9d87d91ca475e9eec
dudushy <eduardotalarico1337@gmail.com>
2022-08-30 23:02:36 -0300

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,11 @@
-console.log("[template-web] Hello World!");
+console.log("[strong-password-generator] init");
+
+function generatePassword() {
+  var text = document.getElementById("password-text");
+  console.log("[generatePassword] text", text);
+
+  const password = Math.floor(Math.random() * (17 - 0 + 1) + 0);
+  console.log("[generatePassword] password", password);
+
+  text.innerText = password;
+}
