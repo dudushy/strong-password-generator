409c2de4c24a542747e49ea05cdaa210a4394f9f
dudushy <eduardotalarico1337@gmail.com>
2022-09-01 18:15:53 -0300

    Working version

--- docs/script.js
+++ docs/script.js
@@ -1,11 +1,19 @@
 console.log("[strong-password-generator] init");
 
 function generatePassword() {
+  const chars = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789-_=!@#%()[].,/\\"
+  console.log("[generatePassword] chars", chars);
+
   var text = document.getElementById("password-text");
   console.log("[generatePassword] text", text);
 
-  const password = Math.floor(Math.random() * (17 - 0 + 1) + 0);
-  console.log("[generatePassword] password", password);
+  var password = "";
+  for (let i = 0; i < 18; i++) {
+    console.log("[generatePassword] i", i);
+    password += chars[Math.floor(Math.random() * chars.length)];
+    console.log("[generatePassword] password", password);
+  }
+  console.log("[generatePassword] FINAL password", password);
 
   text.innerText = password;
 }
